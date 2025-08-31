4583daa55a96fb57168ea5023e9f76296696e7b2
Shayan Shahbazi <upm200@gmail.com>
2025-08-31 18:31:33 +0330

    j01

---
+++ j01/assets/js/script.js
@@ -0,0 +1,32 @@
+let x = 5;
+let y = 5;
+let m = 4.4
+let z = x + y;
+let u = 'ali';
+let b = x * y;
+let c = x * u
+let e = true;
+let v = false;
+let d = "20"
+let r = x ** y;
+
+
+
+var t = 49;
+var t = 55;
+
+console.log(typeof t);
+
+console.log(t);
+console.log(r);
+console.log("hello")
+console.log("x: ", x);
+console.log("y: ", y);
+console.log("z: ", z);
+console.log("u: ", u);
+
+let o = 50;
+o = 70;
+console.log(o);
+
+const p = 10;
