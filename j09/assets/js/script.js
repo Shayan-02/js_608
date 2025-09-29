e99286906894efbcc4dd119d901b8e97c53cf7ae
Shayan Shahbazi <upm200@gmail.com>
2025-09-29 20:11:50 +0330

    j09

--- j09/assets/js/script.js
+++ j09/assets/js/script.js
@@ -1,1 +1,9 @@
 // Add your JavaScript code here
+let paragraph = document.getElementById("heading1");
+
+// paragraph.innerHTML = "salam";
+paragraph.textContent = "salam";
+paragraph.style.color = "blue";
+paragraph.style.fontSize = "60px";
+paragraph.style.backgroundColor = "#9876";
+
