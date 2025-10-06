130fd9d759176a3d5a857e6e5697fcb22cd823e2
Shayan Shahbazi <upm200@gmail.com>
2025-10-06 15:22:35 +0330

    j10

---
+++ j10/1.py
@@ -0,0 +1,5 @@
+def Jalali(num):
+    dice = {1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1}
+    return dice[num]
+
+print(Jalali(int(input())))
---
+++ j10/assets/js/script.js
@@ -0,0 +1,7 @@
+// Add your JavaScript code here
+
+let username = prompt("نام کاربری");
+let password = prompt("رمز عبور");
+
+let s = ["a"]
+let n = [1, 2,]
